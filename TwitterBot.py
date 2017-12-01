8e28e0efed52d986ee560c1c6a93a332a2e8845e
nleeds <nleeds@ucsc.edu>
2017-11-30 18:01:27 -0800

    Reddit credentials now work + added a few comments
    The problem with the Reddit credentials was that we had user_agent='twitterbotUCSC' but we were not allowed to have the word "bot" in the name so I changed it to user_agent='twitterUCSC'

--- TwitterBot.py
+++ TwitterBot.py
@@ -2,28 +2,30 @@ from twython import Twython
 import praw
 import time
 
-APP_KEY = 'UU5q3kRrcYDf0LVr6aeXNk9OX'
-APP_SECRET = 'NMt0hoFsTdzUJhDAU9nKPI0WAXmf4vcAaERixPeEKZe6u0UAE1'
-OAUTH_TOKEN = '936316947947331584-1SVgp23nIBlMziITZp59q8Jea1pmflp'
-OAUTH_TOKEN_SECRET = '5uYvL0LtKOYnVaFo8topsI1ISNDt7SIVASOv8VfW8YCEZ'
+# Twitter credentials
+app_key = 'UU5q3kRrcYDf0LVr6aeXNk9OX'
+app_secret = 'NMt0hoFsTdzUJhDAU9nKPI0WAXmf4vcAaERixPeEKZe6u0UAE1'
+oauth_token = '936316947947331584-1SVgp23nIBlMziITZp59q8Jea1pmflp'
+oauth_token_secret = '5uYvL0LtKOYnVaFo8topsI1ISNDt7SIVASOv8VfW8YCEZ'
+twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 
-twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+# Reddit credentials
+reddit = praw.Reddit(client_id='IdyNhUICtFuUPA',
+                     client_secret='qS_R9mGDhDe8HJfEtsscim6FQKQ',
+                     username='twitterbotUCSC',
+                     password='UCSCslugs420',
+                     user_agent='twitterUCSC')
 
-reddit = praw.Reddit(client_id = 'E8cMmZx6DvPs2A',
-                     client_secret = 'xP1GI9UZP-337Kmmw5dfcvi5qd8',
-                     username = 'RedditSlug',
-                     password = 'cookies',
-                     user_agent = 'test by /u/RedditSlug')
-
-
+# chooses which subreddit to pull data from
 subreddit = reddit.subreddit('Showerthoughts')
 
+# infinite loop that attempts to tweet every 10 minutes
 while(True):
-    new_thought = subreddit.new(limit=1)
+    new_tweet = subreddit.new()
     try:
-        twitter.update_status(status=new_thought.next().title)
+        twitter.update_status(status=new_tweet.next().title)
         print("tweeted")
     except:
         print("no new tweet")
-    time.sleep(60)
-
+    time.sleep(600)
+    
